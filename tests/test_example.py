d88909e8f5b86e0873954847d79e832e3d34d01b
edayot <pro.e.dayot@gmail.com>
2024-08-06 17:47:51 +0200

    examples

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,11 +2,17 @@ import os
 
 import pytest
 from pytest_insta import SnapshotFixture
-from beet import run_beet, PluginError
+from beet import run_beet, PluginError, ProjectConfig, DataPack, ResourcePack
+from beet.contrib.unknown_files import UnknownData
 
 from weld_deps.main import PackNotFoundError, PackVersionNotFoundError
 
 EXAMPLES = [f for f in os.listdir("examples") if not f.startswith("nosnap_")]
+
+
+def remove_unknown_files(pack: DataPack | ResourcePack):
+    for path in set(pack[UnknownData].keys()):
+        del pack[UnknownData][path]
 
 
 @pytest.mark.parametrize("directory", EXAMPLES)
@@ -22,10 +28,26 @@ def test_build(snapshot: SnapshotFixture, directory: str):
             case _:
                 raise ValueError(f"Unknown exception {exception}")
     
-        with pytest.raises(PluginError):
-            with run_beet(directory=f"examples/{directory}") as ctx:
-                pass
+        with pytest.raises(real_exception):
+            try:
+                with run_beet(directory=f"examples/{directory}") as ctx:
+                    pass
+            except PluginError as e:
+                cause = e.__cause__
+                if not isinstance(cause, real_exception):
+                    raise e from None
+                raise cause from None
+            
     else:
         with run_beet(directory=f"examples/{directory}") as ctx:
-            assert snapshot("data_pack") == ctx.data
-            assert snapshot("resource_pack") == ctx.assets
+            data_pack = snapshot("data_pack")
+            resource_pack = snapshot("resource_pack")
+            if isinstance(data_pack, DataPack):
+                remove_unknown_files(data_pack)
+            if isinstance(resource_pack, ResourcePack):
+                remove_unknown_files(resource_pack)
+            remove_unknown_files(ctx.data)
+            remove_unknown_files(ctx.assets)
+
+            assert data_pack == ctx.data
+            assert resource_pack == ctx.assets
