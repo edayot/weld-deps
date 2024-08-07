fd9df8c41e5919c2c010c5f6084832a4270b868b
edayot <pro.e.dayot@gmail.com>
2024-08-07 12:41:59 +0200

    chore: update test case

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,17 +2,17 @@ import os
 
 import pytest
 from pytest_insta import SnapshotFixture
-from beet import run_beet, PluginError, ProjectConfig, DataPack, ResourcePack
-from beet.contrib.unknown_files import UnknownData
+from beet import run_beet, PluginError, ProjectConfig, DataPack, ResourcePack, Pack, NamespaceFile
 
 from weld_deps.main import PackNotFoundError, PackVersionNotFoundError
 
 EXAMPLES = [f for f in os.listdir("examples") if not f.startswith("nosnap_")]
 
 
-def remove_unknown_files(pack: DataPack | ResourcePack):
-    for path in set(pack[UnknownData].keys()):
-        del pack[UnknownData][path]
+def remove_not_vanilla_namespacefile(pack: Pack):
+    for namespace in pack.extend_namespace:
+        for path in set(pack[namespace].keys()):
+            del pack[namespace][path]
 
 
 @pytest.mark.parametrize("directory", EXAMPLES)
@@ -42,12 +42,8 @@ def test_build(snapshot: SnapshotFixture, directory: str):
         with run_beet(directory=f"examples/{directory}") as ctx:
             data_pack = snapshot("data_pack")
             resource_pack = snapshot("resource_pack")
-            if isinstance(data_pack, DataPack):
-                remove_unknown_files(data_pack)
-            if isinstance(resource_pack, ResourcePack):
-                remove_unknown_files(resource_pack)
-            remove_unknown_files(ctx.data)
-            remove_unknown_files(ctx.assets)
+            remove_not_vanilla_namespacefile(ctx.data)
+            remove_not_vanilla_namespacefile(ctx.assets)
 
             assert data_pack == ctx.data
             assert resource_pack == ctx.assets
